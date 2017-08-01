80d4c7328ddb916be038b300d02b499d5e94f5d7
Mateusz Roman <juniorjozek@o2.pl>
2017-08-01 22:00:37 +0200

    Test funkcjonalny teraz sprawdza mozliwosc dodania elementu listy

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -16,24 +17,35 @@ class NewVisitorTest(unittest.TestCase):
 
         # tytul 'Listy' w naglowku strony
         self.assertIn('Listy', self.browser.title)
-        self.fail('Zakonczenie testu!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Listy', header_text)
         # "Tytul okna przegladarki: " + browser.title
 
         # Od razu zostaje zachęcona, aby wpisać rzecz do zrobienia.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Wpisz rzecz do zrobienia'
+        )
 
-# W polu tekstowym wpisała "Kupić pawie pióra" (hobby Edyty # polega na tworzeniu ozdobnych przynęt).
+        # W polu tekstowym wpisała "Kupić pawie pióra" (hobby Edyty # polega na tworzeniu ozdobnych przynęt).
+        inputbox.send_keys('Kupic pawie piora')
+        # Po naciśnięciu klawisza Enter strona została uaktualniona i wyświetla # "1: Kupić pawie pióra" jako element listy rzeczy do zrobienia.
+        inputbox.send_keys(keys.ENTER)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Kupic pawie piora' for row in rows)
+        )
+                # Na stronie nadal znajduje się pole tekstowe zachęcające do podania kolejnego zadania.
+        # Edyta wpisała "Użyć pawich piór do zrobienia przynęty" (Edyta jest niezwykle skrupulatna).
+        self.fail('Zakonczenie testu!')
+        # Strona została ponownie uaktualniona i teraz wyświetla dwa elementy na liście rzeczy do zrobienia.
 
-# Po naciśnięciu klawisza Enter strona została uaktualniona i wyświetla # "1: Kupić pawie pióra" jako element listy rzeczy do zrobienia.
-
-# Na stronie nadal znajduje się pole tekstowe zachęcające do podania kolejnego zadania.
-# Edyta wpisała "Użyć pawich piór do zrobienia przynęty" (Edyta jest niezwykle skrupulatna).
-
-# Strona została ponownie uaktualniona i teraz wyświetla dwa elementy na liście rzeczy do zrobienia.
-
-# Edyta była ciekawa, czy witryna zapamięta jej listę. Zwróciła uwagę na wygenerowany dla niej # unikatowy adres URL, obok którego znajduje się pewien tekst z wyjaśnieniem
+        # Edyta była ciekawa, czy witryna zapamięta jej listę. Zwróciła uwagę na wygenerowany dla niej # unikatowy adres URL, obok którego znajduje się pewien tekst z wyjaśnieniem
 
 
-# Przechodzi pod podany adres URL i widzi wyświetloną swoją listę rzeczy do zrobienia. # Usatysfakcjonowana kładzie się spać.
+        # Przechodzi pod podany adres URL i widzi wyświetloną swoją listę rzeczy do zrobienia. # Usatysfakcjonowana kładzie się spać.
 
 #browser.quit()
 if __name__ == '__main__':
